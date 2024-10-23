337b90ea13af3803bec99e6a9ffbe91357cbbe69
Moldir <144224644+huanhou@users.noreply.github.com>
2024-10-23 23:37:33 +0500

    Delete home.js

--- home.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu");
-  const menuIcon = document.querySelector(".menu-icon");
-  const closeIcon = document.querySelector(".close-icon");
-
-  menu.classList.toggle("open");
-  menuIcon.classList.toggle("open");
-  closeIcon.classList.toggle("open");
-}
